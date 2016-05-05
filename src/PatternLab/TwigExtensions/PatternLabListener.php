ce7cef0ca5d45cce87c2041473cacedf9bed2770
Dave Olsen <dmolsen@gmail.com>
2016-05-04 21:26:52 -0400

    initial commit

---
+++ src/PatternLab/TwigExtensions/PatternLabListener.php
@@ -0,0 +1,43 @@
+<?php
+
+/*!
+ * Twig Extensions Listener Class
+ *
+ * Copyright (c) 2016 Dave Olsen, http://dmolsen.com
+ * Licensed under the MIT license
+ *
+ * Adds Twig Extensions support to the Twig Pattern Engine
+ *
+ */
+
+namespace PatternLab\TwigExtensions;
+
+use \PatternLab\PatternEngine\Twig\TwigUtil;
+
+class PatternLabListener extends \PatternLab\Listener {
+  
+  /**
+  * Add the listeners for this plug-in
+  */
+  public function __construct() {
+    
+    $this->addListener("twigPatternLoader.customize","addExtensions");
+    
+  }
+  
+  /**
+  * Add the extensions to the appropriate instance
+  */
+  public function addExtensions() {
+    
+    $instance = TwigUtil::getInstance();
+    $instance->addExtension(new \Twig_Extensions_Extension_Text());
+    $instance->addExtension(new \Twig_Extensions_Extension_I18n());
+    $instance->addExtension(new \Twig_Extensions_Extension_Intl());
+    $instance->addExtension(new \Twig_Extensions_Extension_Array());
+    $instance->addExtension(new \Twig_Extensions_Extension_Date());
+    TwigUtil::setInstance($instance);
+    
+  }
+  
+}
